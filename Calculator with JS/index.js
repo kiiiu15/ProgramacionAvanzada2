5d13fff5b85997e6681cfdd12dc9f42a925d72ae
kiiiu15 <tinbulzomi@hotmail.com>
2020-08-13 18:07:20 -0300

    Calculator Interface

---
+++ Calculator with JS/index.js
@@ -0,0 +1,20 @@
+function add (number1, number2){
+    return Number(number1) + Number(number2); 
+}
+
+
+function subtract (number1, number2){
+    return Number(number1) - Number(number2); 
+}
+
+function multiply (number1, number2){
+    return Number(number1) * Number(number2); 
+}
+
+
+function divide (number1, number2){
+    return Number(number1) / Number(number2); 
+}
+
+
+
