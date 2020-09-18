7ea4660a89473cf49b9b3c3aefde632cb95e933d
kiiiu15 <tinbulzomi@hotmail.com>
2020-09-17 21:11:26 -0300

    Ex 1 GetElement

---
+++ DOM Manipulation/With getElement/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Insertar el string "Hello World" dentro del elemento <p>
+const text = "Hello world!";
+const elementP = document.getElementById("myId");
+elementP.innerHTML = text;
+
