5472a8df3d94796aa2893496e70ecb9b339bba12
kiiiu15 <tinbulzomi@hotmail.com>
2020-09-17 21:19:54 -0300

    Ex2 getElement

--- DOM Manipulation/With getElement/index.js
+++ DOM Manipulation/With getElement/index.js
@@ -5,3 +5,10 @@ const text = "Hello world!";
 const elementP = document.getElementById("myId");
 elementP.innerHTML = text;
 
+// Insertar el string "Hello World" dentro del primer elemento que tenga la clase test
+
+const pArray = document.getElementsByClassName("test");
+const firstP = pArray[0];
+firstP.innerHTML = text;
+
+
