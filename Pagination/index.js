e759f1c1077902b087312b103a0633517a44e0e6
kiiiu15 <tinbulzomi@hotmail.com>
2020-09-20 17:43:37 -0300

    Pagination, No pagination

--- Pagination/index.js
+++ Pagination/index.js
@@ -1,23 +1,30 @@
 console.log("Hello world!");
 const API_URL = "https://utn-avanzanda2-tp5.herokuapp.com";
-retrieveData()
-.then(data => {
-    console.log(JSON.stringify(data, null, 2));
-})
-.catch(error => {
-    console.log(error);
-});
+const tbody = document.querySelector('#myTbody');
+const tableHead = [
+    "userId",
+    "firstName",
+    "lastName",
+    "email",
+    "gender",
+    "lastConnectedAddress"
+];
+tbody.innerHTML = "";
 
+function prepareRequest(url) {
+    const req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.responseType = "json";
+    return req;
+}
 
-
-
-function retrieveData(url = API_URL+"/api/User") {
+function retrieveData(url = API_URL + "/api/User") {
     return new Promise((resolve, reject) => {
-        
+
         const req = prepareRequest(url);
 
         req.onload = function () {
-            if (req.status == 200) {
+            if (req.status >= 200 && req.status < 300) {
                 resolve(req.response)
             } else {
                 reject(Error("Failed retriving data! " + req.statusText));
@@ -32,10 +39,33 @@ function retrieveData(url = API_URL+"/api/User") {
     });
 }
 
-function prepareRequest(url){
-    const req = new XMLHttpRequest();
-    req.open("GET", url);
-    req.responseType = "json";
-    return req;
+
+
+
+function treatment(data) {
+    data.forEach(element => {
+       let row = createLine(element);
+       tbody.appendChild(row);
+    });
 }
 
+function createLine(element) {
+    let tr = document.createElement("tr");
+    tableHead.forEach(element2 => {
+        let td = document.createElement("td");
+        td.innerHTML = element[element2];
+        tr.appendChild(td);
+    });
+
+    return tr;
+
+}
+
+function refreshTable() {
+    retrieveData()
+        .then(treatment)
+        .catch(error => console.log(error));
+}
+
+
+refreshTable();
